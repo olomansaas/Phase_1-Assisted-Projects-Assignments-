71debe96bc8f9acf695b2bcd5fd5ed6c9254d69f
olomansaas <148766801+olomansaas@users.noreply.github.com>
2023-10-31 17:08:08 +0530

    Add files via upload

--- Trycatch.java
+++ Trycatch.java
@@ -21,19 +21,59 @@ import java.io.IOException;
 //}
 
 
-public class Trycatch {
-
-	public static void main(String[] args) throws IOException {
-		int i=2;
-		int j=0;
-		
-		try {
-			int k=i/j;
-		}
-		catch(ArithmeticException e) {
-			System.out.println("j can not be zero");
+/*	public class Trycatch {
+	
+		public static void main(String[] args) throws IOException {
+			int i=2;
+			int j=0;
 			
+			try {
+				int k=i/j;
+			}
+			catch(ArithmeticException e) {
+				System.out.println("j can not be zero");
+				
+			}
 		}
+	
 	}
+*/
 
-}
+
+
+/*
+ * public class Trycatch {
+ * 
+ * public static void main(String[] args) { int i=2; int j=0;
+ * 
+ * try { int k=i/j; } catch(ArithmeticException e) {
+ * System.out.println("j can not be zero");
+ * 
+ * }
+ * 
+ * finally { System.out.println("finally bye"); } }
+ * 
+ * }
+ */
+
+
+
+
+
+/*
+ * public class Trycatch {
+ * 
+ * public static void main(String[] args) { int i=2; int j=0;
+ * 
+ * try { int k=i/j; } catch(ArithmeticException e) {
+ * System.out.println("j can not be zero");
+ * 
+ * } catch(Exception e) { System.out.println("unexpected exception");
+ * 
+ * } }
+ * 
+ * }
+ */
+
+
+
